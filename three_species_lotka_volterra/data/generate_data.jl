b876cb4e68df8a4f12d7586dacaeaec413275a21
Nina Schmid <nina.schmid@uni-bonn.de>
2023-06-21 15:12:37 +0200

    add noisy and sparse data

--- three_species_lotka_volterra/data/generate_data.jl
+++ three_species_lotka_volterra/data/generate_data.jl
@@ -1,6 +1,10 @@
 using DifferentialEquations
 using Plots
 using DelimitedFiles
+using Statistics
+using Distributions
+using Random
+Random.seed!(42)
 
 include("../reference.jl")
 
@@ -18,7 +22,7 @@ end
 tspan = (0.0, 10.0)
 t = tspan[1]:0.05:tspan[end]
 
-prob = ODEProblem(dynamics!,u0, tspan, parameter_values)
+prob = ODEProblem(dynamics!, IC, tspan, parameter_values)
 sol = solve(prob, Tsit5(), reltol=1e-8, saveat = t)
 sol = Array(sol)
 
@@ -36,3 +40,38 @@ plot!(t, sol[3,:], label="u3", xlabel="time", color=3)
 savefig(plt, joinpath("reference.png")) 
 
 # Simulate noisy and sparse data
+for n_datapoints in [40, 80]
+    for noise_level in [0, 5, 15]
+
+        # create data
+        delta_t = tspan[end]/n_datapoints
+        t_sparse = tspan[1]:delta_t:tspan[end]
+        prob = ODEProblem(dynamics!,IC, tspan, parameter_values)
+        sol_sparse = solve(prob, Tsit5(), reltol=1e-8, saveat = t_sparse)
+        sol_sparse = Array(sol_sparse)
+        
+        # potentially add noise
+        if noise_level!=0
+            noise = mean(sol_sparse, dims=2)*noise_level/100
+            d = Normal.(0, noise)
+            ϵ = transpose(hcat(rand.(d, n_datapoints+1)...))
+            sol_sparse = sol_sparse + ϵ
+        end
+
+        # store results
+        open(joinpath("lotka_volterra_datapoints_$(n_datapoints)_noise_$(noise_level).csv"), "w") do io
+            header = ["t" states...]
+            writedlm(io, header, ",")
+            writedlm(io, [t_sparse transpose(sol_sparse)], ",")
+        end
+
+        # plot results
+        plt = plot(t, sol[1,:], label="u1", color=1)
+        plot!(t, sol[2,:], label="u2", color=2)
+        plot!(t, sol[3,:], label="u3", xlabel="time", color=3)
+        scatter!(t_sparse, sol_sparse[1,:], label=missing, markersize=2, color=1)
+        scatter!(t_sparse, sol_sparse[2,:], label=missing, markersize=2, color=2)
+        scatter!(t_sparse, sol_sparse[3,:], label=missing, markersize=2, xlabel="time", color=3)
+        savefig(plt, joinpath("datapoints_$(n_datapoints)_noise_$(noise_level).png")) 
+    end
+end
